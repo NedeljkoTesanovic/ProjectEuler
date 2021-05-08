cad49a4e5dfa1cc150f7ac9a18c97d8a9c2b0298
Недељко Тешановић <69245776+NedeljkoTesanovic@users.noreply.github.com>
2021-05-08 21:45:39 +0200

    +Efficiency
    Improved division by 2;
    Made the divider skip even numbers;

--- p3.cpp
+++ p3.cpp
@@ -1,16 +1,17 @@
 /*
 The prime factors of 13195 are 5, 7, 13 and 29.
-
 What is the largest prime factor of the number 600851475143 ?
 */
 
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    ulong num = 600851475143;
-    ulong p = 2;
+    unsigned long long num = 600851475143;
+    unsigned long p = 2;
+    while(num % p == 0)
+        num>>=1; //Primes are not even numbers - divide by 2 as long as you can; Gets rids of multiples of 2 as factors;
+    p++; //what's left are odd factors, so start with 3 and do the same thing.
     while(num != 1)
     {
         if(num % p == 0)
@@ -18,8 +19,8 @@ int main()
             num/=p;
         }
         else
-             p++;
-    }
-    cout<<p<<endl;
+             p+=2;
+    } //The remaining factor is hence the biggest prime number
+    cout<<"Prime: "<<p<<endl;
     return 0;
 }
